3a538c5c55795dd1329c457efc85308ffa4be367
PaulODonovan <paul@paulodonovan.net>
2014-04-14 10:36:08 +0200

    slider & spinner bound. Bug: css jumps

--- js/slider.js
+++ js/slider.js
@@ -46,19 +46,18 @@ angular.module('app', [])
 .directive('spinner', function () {
 	return {
 		restrict: 'E',
-		template: '<span id="spin"> €<input type="number" ng-model="preview.donation" value="{{preview.donation}}" max="10000" min="0" step="5"></span>',
+		template: '<span> €<input id="spinner" type="number" ng-model="preview.donation" value="{{preview.donation}}" max="10000" min="0" step="5"></span>',
 		replace: true,
-		// scope: {
-		// 	project: '='
-		// },
 		link: function ( scope, elem, attr ) {
 			scope.$watch('preview.donation', function () {
 				console.log("spinner says scope.preview.donation : " + scope.preview.donation);
-			// 	// set the project donation
-			// 	scope.project.donation = scope.preview.donation * scope.project.ration
-			// 	console.log("scope.project.donation for " + scope.project.name + " : " + scope.project.donation);
-			// 	// set the project bar fill widths
-			// 	scope.percentage = (scope.project.donation + scope.project.raised) / scope.project.target * 100
+
+				// This is to update the slider. There is a bug where changing the slider after the spinner causes a jump, probably to do with how the powerange slider calculates it's css.
+				var spinnerVal = document.getElementById('spinner').value;
+				var newCss = spinnerVal / 100 + "%";
+				console.log("spinnerVal :" + spinnerVal);
+				document.querySelector('.range-quantity').style.width = newCss;
+				document.querySelector('.range-handle').style.left = newCss;
 			});
 		}
 	};
@@ -73,15 +72,20 @@ angular.module('app', [])
 		restrict: 'E',
 		template: '<input type="text" class="js-range" ng-model="preview.donation"/>',
 		replace: true,
-		// scope: {
-		// 	project: '='
-		// },
 		link: function ( scope, elem, attr ) {
 			elem = elem[0]
 			new Powerange(elem, {
 				min: 0,
 				max: 10000,
 				start: scope.preview.donation || 0,
+				//hideRange: true
+			});
+
+			scope.$watch('preview.donation', function () {
+				//elem.value = scope.preview.donation;
+				console.log("slider says scope.preview.donation : " + scope.preview.donation);
+				console.log(" slider says elem.value: " + elem.value);
+				document.getElementById('spinner').value = elem.value;
 			});
 		}
 	};
@@ -93,9 +97,6 @@ angular.module('app', [])
 		restrict: 'E',
 		template: '<div class="bar"><div class="barFill" style="width:{{ percentage }}%;"></div></div>',
 		replace: true,
-		// scope: {
-		// 	project: '='
-		// },
 		link: function ( scope, element, attr ) {
 			scope.$watch('preview.donation', function () {
 				console.log("progressBar says scope.preview.donation : " + scope.preview.donation);
@@ -108,3 +109,7 @@ angular.module('app', [])
 		}
 	};
 });
+
+
+
+
